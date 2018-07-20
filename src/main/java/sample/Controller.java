cf8382881dd0b5e52965bc60f5b0e7c0c3842f8f
Vishnu Rainigari <vrainigari@cardinalpeak.com>
2018-07-20 16:31:13 -0700

    initial

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void pressButton(ActionEvent event){
+        System.out.println("Hello world");
+    }
+
+    public void
+}
